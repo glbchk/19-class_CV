f75f899067bb5225edc11ce39d604b2b78e6ceca
glbchk <glegalchenko@gmail.com>
2022-11-01 00:56:26 -0300

    Created a project

---
+++ CV/CV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CV
+//
+//  Created by Glib Galchenko on 31.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
